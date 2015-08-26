4d784da1d9e8d78026cb0cf24fb4acf95545d883
Austin Gilbert <ceretullis@gmail.com>
2015-08-26 17:24:17 -0500

    When more than 1 link_lib is provided the if() fails, fixing.

--- AddLibrary/ExecutableFunction.cmake
+++ AddLibrary/ExecutableFunction.cmake
@@ -6,7 +6,7 @@ function(MAKE_EXECUTABLE EXEC_NAME)
 	set(dependencies ${PARSED_ARGS_DEPENDENCIES})
 	set(link_libs ${PARSED_ARGS_LINK_DEPS})
 
-    add_source(${CMAKE_CURRENT_SOURCE_DIR} implementation)
+    	add_source(${CMAKE_CURRENT_SOURCE_DIR} implementation)
 
     # create the library target
 	add_executable(${executable_name}
@@ -18,7 +18,14 @@ function(MAKE_EXECUTABLE EXEC_NAME)
 		add_dependencies(${executable_name} ${dependencies})
 	endif()
 
-	if(${link_libs})
-		target_link_libraries("${executable_name}" ${link_libs})
+	set(all_deps ${dependencies} ${platform_dependencies} ${link_libs})
+	list(LENGTH all_deps hasLinkLibs)
+	
+	if(hasLinkLibs GREATER 0)
+		target_link_libraries(
+			"${executable_name}" 
+			${dependencies} 
+			${platform_dependencies}
+			${link_libs})
 	endif()
 endfunction()
