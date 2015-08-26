102f30571e10cc8bd30c71a4783a2c482a05394a
Austin Gilbert <ceretullis@gmail.com>
2015-08-26 17:05:52 -0500

    Resolved conflicts with origin/develop

--- AddLibrary/ExecutableFunction.cmake
+++ AddLibrary/ExecutableFunction.cmake
@@ -6,7 +6,7 @@ function(MAKE_EXECUTABLE EXEC_NAME)
 	set(dependencies ${PARSED_ARGS_DEPENDENCIES})
 	set(link_libs ${PARSED_ARGS_LINK_DEPS})
 
-    add_source(${CMAKE_CURRENT_SOURCE_DIR} implementation)
+    	add_source(${CMAKE_CURRENT_SOURCE_DIR} implementation)
 
     # create the library target
 	add_executable(${executable_name}
@@ -19,6 +19,10 @@ function(MAKE_EXECUTABLE EXEC_NAME)
 	endif()
 
 	if(${link_libs})
-		target_link_libraries("${executable_name}" ${link_libs})
+		target_link_libraries(
+			"${executable_name}" 
+			${dependencies} 
+			${platform_dependencies}
+			${link_libs})
 	endif()
 endfunction()
