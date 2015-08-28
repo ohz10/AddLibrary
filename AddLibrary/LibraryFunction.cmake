0d25bbd8bd03fe3bde5fd2f9e903f12512e64dac
Austin Gilbert <ceretullis@gmail.com>
2015-08-28 17:01:04 -0500

    Add support for library/executable specific compiler flags. These flags will get added to any other flags previously added at the project level. This can be used to suppress warnings triggered by a specific build lib/exec for example.

--- AddLibrary/LibraryFunction.cmake
+++ AddLibrary/LibraryFunction.cmake
@@ -1,12 +1,16 @@
 function(MAKE_LIBRARY LIB_NAME)
 	set(library_name "${LIB_NAME}")
 	
-	cmake_parse_arguments(PARSED_ARGS "" "NAME" "DEPENDENCIES;UNITTEST_LIBS;GENERATED_SOURCE_FILES;GENERATED_TESTING_SOURCE_FILES;GENERATED_UT_SOURCE_FILES" ${ARGN})
+	cmake_parse_arguments(PARSED_ARGS "" "NAME" "DEPENDENCIES;UNITTEST_LIBS;GENERATED_SOURCE_FILES;GENERATED_TESTING_SOURCE_FILES;GENERATED_UT_SOURCE_FILES;COMPILE_FLAGS;TESTING_COMPILE_FLAGS;UT_COMPILE_FLAGS" ${ARGN})
 	set(dependencies ${PARSED_ARGS_DEPENDENCIES})
 	set(unit_test_dependencies ${PARSED_ARGS_UNITTEST_LIBS})
 	set(generated_source_files ${PARSED_ARGS_GENERATED_SOURCE_FILES})
 	set(generated_testing_source_files ${PARSED_ARGS_GENERATED_TESTING_SOURCE_FILES})
 	set(generated_ut_source_files ${PARSED_ARGS_GENERATED_UT_SOURCE_FILES})
+	
+	set(compiler_flags ${PARSED_ARGS_COMPILE_FLAGS})
+	set(testing_compiler_flags ${PARSED_ARGS_TESTING_COMPILE_FLAGS})
+	set(ut_compiler_flags ${PARSED_ARGS_UT_COMPILE_FLAGS})
 
 	message("Adding Library '${library_name}'")
 	
@@ -15,6 +19,10 @@ function(MAKE_LIBRARY LIB_NAME)
 		source_files 
 		FILTER_DIRS "tests" "testing")
 	add_library(${library_name} STATIC ${source_files} ${generated_source_files})
+
+	foreach(flag ${compiler_flags})
+		set_property(TARGET ${library_name} APPEND_STRING PROPERTY COMPILE_FLAGS "${flag} ")
+	endforeach(flag)
 
 	if(${dependencies})
 		add_dependencies(${library_name} ${dependencies})
@@ -29,6 +37,10 @@ function(MAKE_LIBRARY LIB_NAME)
 		add_source("${CMAKE_CURRENT_SOURCE_DIR}/testing/" testing_source)
 		add_library(${testing_lib} ${testing_source} ${generated_testing_source_files})
 
+		foreach(flag ${testing_compiler_flags})
+			set_property(TARGET ${testing_lib} APPEND_STRING PROPERTY COMPILE_FLAGS "${flag} ")
+		endforeach(flag)
+
 		add_dependencies(${testing_lib} ${library_name})
 	endif()
 
@@ -40,6 +52,10 @@ function(MAKE_LIBRARY LIB_NAME)
 
 		add_source("${CMAKE_CURRENT_SOURCE_DIR}/tests/unit_test/" ut_implementation)
 		add_executable(${executable_name} ${ut_implementation} ${generated_ut_source_files})
+
+		foreach(flag ${ut_compiler_flags})
+			set_property(TARGET ${executable_name} APPEND_STRING PROPERTY COMPILE_FLAGS "${flag} ")
+		endforeach(flag)
 
 		add_dependencies(
 			${executable_name}
