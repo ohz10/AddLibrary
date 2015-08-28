ba9f41272476f70f6a07001f4bbeb4fb48fb2fb7
Austin Gilbert <ceretullis@gmail.com>
2015-08-28 17:01:04 -0500

    Add support for library/executable specific compiler flags. These flags will get added to any other flags previously added at the project level. This can be used to suppress warnings triggered by a specific build lib/exec for example.

--- AddLibrary/ExecutableFunction.cmake
+++ AddLibrary/ExecutableFunction.cmake
@@ -2,10 +2,11 @@ function(MAKE_EXECUTABLE EXEC_NAME)
 	set(executable_name "${EXEC_NAME}")
 	message("Adding Executable '${executable_name}'")
 
-	cmake_parse_arguments(PARSED_ARGS "" "NAME" "LINK_DEPS;DEPENDENCIES;GENERATED_SOURCE_FILES" ${ARGN})
+	cmake_parse_arguments(PARSED_ARGS "" "NAME" "LINK_DEPS;DEPENDENCIES;GENERATED_SOURCE_FILES;COMPILE_FLAGS" ${ARGN})
 	set(generated_source_files ${PARSED_ARGS_GENERATED_SOURCE_FILES})
 	set(dependencies ${PARSED_ARGS_DEPENDENCIES})
 	set(link_libs ${PARSED_ARGS_LINK_DEPS})
+	set(compiler_flags ${PARSED_ARGS_COMPILE_FLAGS})
 
     add_source(${CMAKE_CURRENT_SOURCE_DIR} implementation)
 
@@ -14,6 +15,10 @@ function(MAKE_EXECUTABLE EXEC_NAME)
 		${implementation}
 		${generated_source_files}
 	)
+
+	foreach(flag ${compiler_flags})
+			set_property(TARGET ${executable_name} APPEND_STRING PROPERTY COMPILE_FLAGS "${flag} ")
+	endforeach(flag)
 
 	list(LENGTH dependencies hasDependencies)
 	if(hasDependencies GREATER 0)
